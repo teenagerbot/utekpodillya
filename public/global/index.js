75328dd15a0c37677cc576b7f0f73e0fad8e895b
Студент 3 <user3@khnu.km.ua>
2023-10-12 15:35:02 +0300

    A

---
+++ public/global/index.js
@@ -0,0 +1,39 @@
+const menu = document.querySelector('.menu');
+let startX = 0;
+let currentX = 0;
+let isSwiping = false;
+document.querySelector("#menu-burger").onclick = () => {
+    menu.classList.toggle('open');
+    document.querySelector("div.app-locker").style.display = "block";
+}
+document.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+    isSwiping = true;
+});
+
+document.addEventListener('touchmove', (e) => {
+    if (!isSwiping) return;
+    currentX = e.touches[0].clientX;
+    const diffX = currentX - startX;
+
+    if (diffX > 50) {
+        menu.classList.add('open');
+        document.querySelector("div.app-locker").style.display = "block";
+    } else if (diffX < -50) {
+        menu.classList.remove('open');
+        setTimeout(() => {
+            document.querySelector("div.app-locker").style.display = "none";
+        }, 200)
+    }
+});
+
+document.addEventListener('touchend', () => {
+    isSwiping = false;
+});
+document.querySelector("div.app-locker").onclick = () => {
+    menu.classList.toggle('open');
+    document.querySelector("div.app-locker").style.display = "none";
+}
+window.onscroll =() => {
+    scrollFunction()
+}
