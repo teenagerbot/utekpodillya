28d2207d45c749f59837d0509cac42d27fdf0515
developer <91476715+teenagerbot@users.noreply.github.com>
2023-10-19 21:30:47 +0300

    admin

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const cookieManager = require('cookie-parser');
+const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express();
 const sockets = require('socket.io');
@@ -13,14 +16,35 @@ const io = sockets(server, {
 });
 // middlewares
 app.use(express.static('public'))
-// routes
+app.use('/admin', express.static(path.join(__dirname, "public", 'adminPanel')));
+app.use(cookieManager());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/hello', (req, res) => {
 	res.send("hello");
 	res.end();
+});
+app.get("/adminer", (req, res) => {
+	if (req.cookies["admin"] && req.cookies["admin"] === "hello") {
+		res.redirect("/admin")
+	} else {
+		res.status(404)
+		res.end();
+	}
 })
-
-
-
+app.get("/hello/:numbersession", (req, res) => {
+	if (req.params.numbersession === "340") {
+		res.cookie("admin", "hello", {
+			secure: true,
+			httpOnly: true,
+			maxAge: 672 * 60 * 60 * 1000
+		});
+		res.redirect("/adminer");
+		res.end();
+	}
+})
+app.get(/\/?.+/, (req, res) => {
+	console.log(req.url)
+})
 //listen on every connection
 io.on('connection', (socket) => {
 	console.log('New user connected');
@@ -32,20 +56,33 @@ io.on('connection', (socket) => {
 		const prds = fs.readFileSync("./pr.json", "utf-8");
 		io.to(socket.id).emit("products", prds);
 	})
+	socket.on("getFile", (file) => {
+		if (fs.existsSync(`./prodfiles/${file}.html`)) {
+			const Stream = fs.createReadStream(`./prodfiles/${file}.html`, {encoding: "utf-8", highWaterMark: 1024 });
+			Stream.on("data", (chunk) => {
+				io.to(socket.id).emit("addFileChunk", chunk);
+			});
+			Stream.on("end", () => {
+				Stream.destroy();
+			})
+		} else {
+			io.to(socket.id).emit("errorReadProd");
+		}
+	})
 	/**
 		when we sent the file via client
 		client(script.js):
 		let file = document.querySelector("input[type='file']").files[0];
 		io.emit("getFile", file);
 	*/
-	socket.on("getFile", (fileData) => {
-		fs.writeFile(`file.png`, fileData.file, (err) => {
-			if (err) io.to(socket.id).emit("error");
-			else {
-				// ....
-			}
-		});
-	});
+	// socket.on("getFile", (fileData) => {
+	// 	fs.writeFile(`file.png`, fileData.file, (err) => {
+	// 		if (err) io.to(socket.id).emit("error");
+	// 		else {
+	// 			// ....
+	// 		}
+	// 	});
+	// });
 });
 server.listen(3000, "0.0.0.0", function() {
 	console.log('listening on 3000');
--- public/catalog/js/index.js
+++ public/catalog/js/index.js
@@ -12,32 +12,61 @@ Server.on("products", (products) => {
         const card = CreateCard(cat);
         document.querySelector(".view .view-prods").appendChild(card);
     })
+
     document.querySelector("div.view-prods").onclick = (e) => {
         if (e.target.hasAttributes("data-prod")) {
             const ViewAdapter = document.createElement("view");
-            console.log(All,  e.target.getAttribute("data-prod"))
             All[e.target.getAttribute("data-prod")].forEach(prod => {
                 const ViewItem = document.createElement("item");
-                ViewItem.innerHTML = `<div class="mdl-card__actions mdl-card--border">
-        <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-            ${prod.name}
-        </a>
-        <div class="mdl-layout-spacer"></div>
-    </div>`;
+                ViewItem.innerHTML = `<button class="button" detail="${prod.name}">${prod.name}</button><run>></run>`;
                 ViewAdapter.appendChild(ViewItem);
             })
-            document.querySelector(".view .view-prod").appendChild(ViewAdapter);
+            document.querySelector(".view .view-prod .viewrdk").appendChild(ViewAdapter);
             document.querySelector(".view .view-prod").style.height = "100%";
             document.querySelector(".view .view-prods").style.height = "0%";
             document.querySelector(".view .view-prods").style.display = "none";
+            document.querySelector(".view .view-prod").style.display = "block";
+            if (document.querySelector(".view .view-prod .viewrdk view").innerHTML === "") {
+                // Region translate
+                document.querySelector(".view .view-prod .viewrdk").innerHTML = `<h2 id="none">Тут поки що нічого нема</h2>`;
+                // End translate
+            }
+            document.querySelector(".viewrdk view").onclick = (ev) => {
+                if (ev.target.hasAttributes("detail")) {
+                    Server.emit("getFile", ev.target.getAttribute("detail"));
+                }
+            }
         }
     }
+    document.querySelector("#back").onclick = () => {
+        document.querySelector(".view .view-prod").style.height = "0%";
+        document.querySelector(".view .view-prods").style.height = "100%";
+        document.querySelector(".view .view-prods").style.display = "flex";
+        document.querySelector(".view .view-prod").style.display = "none";
+        document.querySelector(".view .view-prod .viewrdk").innerHTML = "";
+    }
 })
+Server.on("addFileChunk", content => {
+    document.querySelector(".view-file .webview").innerHTML += content;
+    document.querySelector(".view-file").style.display = "flex";
+    document.querySelector(".app-locker").style.opacity = 1;
+    document.querySelector(".app-locker").style.pointerEvents = "auto";
+})
+let tltp = new Tooltip();
+Server.on("errorReadProd", () => {
+    tltp.error("Нема продуктів", "top", 5, true)
+})
+document.querySelector("#close").onclick = () => {
+    document.querySelector(".view-file").style.display = "none";
+    document.querySelector(".view-file .webview").innerHTML = "";
+    document.querySelector(".app-locker").style.opacity = 0;
+    document.querySelector(".app-locker").style.pointerEvents = "none";
+}
 function CreateCard(content) {
     const card = document.createElement("div");
     card.className = "demo-card-event mdl-card mdl-shadow--2dp";
     card.innerHTML = `<div class="mdl-card__title mdl-card--expand"><h4>
-        ${String(content).replace(/~\|\d.+\|~/gm, "")}
+        ${String(content).replace(/~\-\d.+\-~/gm, "")}
     </h4></div><div class="mdl-card__actions mdl-card--border">
     <button class="button android-button" data-prod="${String(content)}">
             Розгорнути
